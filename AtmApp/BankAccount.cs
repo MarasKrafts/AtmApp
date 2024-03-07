437b1561b849c34cfdfcf99896f8cf4eedcaca71
MarasKrafts <goldenprenzy@gmail.com>
2024-03-07 23:57:01 +0100

    Add project files.

---
+++ AtmApp/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atmachine
+{
+    public class BankAccount
+    {
+        private double balance;
+
+        public BankAccount(double initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds.");
+                return false;
+            }
+        }
+    }
+}
+
---
+++ AtmApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Atmachine;
+
+namespace Atmachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double initialBalance = 5000; 
+            BankAccount account = new BankAccount(initialBalance);
+
+            Console.WriteLine("Welcome to the ATM!");
+            Console.WriteLine($"Your initial balance: ₱ {account.GetBalance():N2}");
+
+            
+            account.Deposit(1000);
+            Console.WriteLine($"New balance after deposit: ₱ {account.GetBalance():N2}");
+
+            
+            bool success = account.Withdraw(2000);
+            if (success)
+                Console.WriteLine($"New balance after withdrawal: ₱ {account.GetBalance():N2}");
+
+            
+            account.Withdraw(10000); 
+
+            Console.ReadKey();
+        }
+    }
+}
