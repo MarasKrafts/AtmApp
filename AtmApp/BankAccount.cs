e276d3074f6e620deb5d7ccd85b937d91435d409
MarasKrafts <goldenprenzy@gmail.com>
2024-03-08 13:45:21 +0100

    Not much changes made. Will remake the entire project soon!

--- AtmApp/BankAccount.cs
+++ AtmApp/BankAccount.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Atmachine
+﻿namespace Atmachine
 {
     public class BankAccount
     {
--- AtmApp/Program.cs
+++ AtmApp/Program.cs
@@ -10,16 +10,16 @@ namespace Atmachine
             BankAccount account = new BankAccount(initialBalance);
 
             Console.WriteLine("Welcome to the ATM!");
-            Console.WriteLine($"Your initial balance: ₱ {account.GetBalance():N2}");
+            Console.WriteLine($"Your initial balance: $ {account.GetBalance():N2}");
 
             
             account.Deposit(1000);
-            Console.WriteLine($"New balance after deposit: ₱ {account.GetBalance():N2}");
+            Console.WriteLine($"New balance after deposit: $ {account.GetBalance():N2}");
 
             
             bool success = account.Withdraw(2000);
             if (success)
-                Console.WriteLine($"New balance after withdrawal: ₱ {account.GetBalance():N2}");
+                Console.WriteLine($"New balance after withdrawal: $ {account.GetBalance():N2}");
 
             
             account.Withdraw(10000); 
